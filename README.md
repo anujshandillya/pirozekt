f8f86e6294eb7a1669e3a24772c27d4a045b1885
Anuj Sharma <anujsharmawork11@gmail.com>
2024-04-15 18:16:56 +0530

    dev

---
+++ README.md
@@ -0,0 +1,109 @@
+# DocBot
+
+### Description
+"DocBot" suggests a platform where users can upload PDF documents and ask questions related to the content of those documents. It implies a central hub or repository for querying information from PDF files. Users can search for specific information, extract data, or seek answers to their queries by interacting with the uploaded PDF documents.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+# Getting Started
+## Installation
+
+1. **Clone the Repository**
+    ```sh
+    git clone https://github.com/anujshandillya/pirozekt.git
+    ```
+2. **Create a local env**
+   ### Windows
+   Go to project directory.
+   ```sh
+   cd path\to\your\project
+   ```
+   Create virtual environment.
+   ```sh
+   python -m venv myenv
+   ```
+   Activate the environment.
+   ```sh
+   myenv\Scripts\activate
+   ```
+   ### Linux/MacOS
+   Go to project directory.
+   ```sh
+   cd path\to\your\project
+   ```
+   Create virtual environment.
+   ```sh
+   python -m venv myenv
+   ```
+    Activate the environment.
+   ```sh
+   source myenv/bin/activate
+   ```
+
+3. **Get your Cohere API key**
+    - Go to <a href="http://www.cohere.com/">Cohere</a> and get your own API key.
+
+4. **Create a .env file and load it**
+   Create a file .env
+   ```bash
+   touch .env
+   ```
+   Paste your API key
+   ```
+   API_KEY='<your_api_key>'
+   ```
+   Load it
+   ```py
+   from dotenv import load_dotenv
+   load_dotenv()
+   ```
+
+5. **Install dependencies**
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+- Start the application by:
+    ```bash
+    streamlit run app.py
+    ```
+- Select input type: **PDF**, **TEXT**.
+- Ask questions related to the uploaded document.
+- It will give answer related to the query.
+
+
+### Libraries used:
+1. **cohere**
+   - This module is imported, presumably for specific functionality provided by the 'cohere' library.
+
+2. **NumPy (np)**
+   - NumPy is imported and aliased as 'np'. It is widely used for numerical computing, providing powerful array operations and mathematical functions.
+
+3. **norm**
+    - The 'norm' function is imported from the 'numpy.linalg' module. This function calculates the norm (magnitude) of a vector or matrix.
+
+4. **Pandas (pd)**
+   - Pandas is imported and aliased as 'pd'. It is used for data manipulation and analysis, offering data structures like DataFrame and tools for reading and writing data.
+
+5. **pdfplumber**
+   - This module is imported, likely used for extracting data from PDF documents.
+
+6. **Streamlit (st)**
+   - Streamlit is imported and aliased as 'st'. It is a popular framework for building interactive web applications for data science and machine learning.
+
+7. **load_dotenv (from dotenv)**
+   - The 'load_dotenv' function is imported from the 'dotenv' module. This function is used to load environment variables from a .env file into the environment.
+
+8. **os**
+   - The 'os' module is imported, providing operating system-dependent functionality, such as interacting with the file system and environment variables.
+
+## Contributing
+This project welcomes contributions and suggestions.
+
+## License
+[License](LICENSE)
--- app.py
+++ app.py
@@ -7,19 +7,29 @@ from io import StringIO
 from typing import Sequence
 from dotenv import load_dotenv
 import os
+from numpy.linalg import norm
 
+#  loaded local env
 load_dotenv()
+
 api=os.getenv('API_KEY')
+# default settings for generation of text
+TEMPERATURE = 0.5
+MAX_TOKENS = 200
+text=""
+# result=""
 
 co=cohere.Client(api)
 
-def extractTextFromPdf(pdf_path: str):
+# Get the text from pdf file.
+def extractTextFromPdf(pdfPath: str):
     text = ""
-    with pdfplumber.open(pdf_path) as pdf:
+    with pdfplumber.open(pdfPath) as pdf:
         for page in pdf.pages:
             text += page.extract_text()
     return text
 
+# Creating a dataframe to break information into user defined Chunks.
 def processTextInput(text: str, run_id: str = None):  
     text = StringIO(text).read()  
     CHUNK_SIZE=150
@@ -28,22 +38,44 @@ def processTextInput(text: str, run_id: str = None):
     df = pd.DataFrame.from_dict({'text': chunks}) 
     return df
 
+# Converting the dataframe to list of strings.
 def convertToList(df):
     df['col']=df[['text']].apply(lambda row: ' '.join(row.dropna().astype(str)), axis=1)
     seqOfStrings: Sequence[str]=df['col'].tolist()
     return seqOfStrings
 
+# Using the Cohere embed endpoint to embed the data into vector data.
 def embed(Texts: Sequence[str]):
     res=co.embed(texts=Texts, model="small")
     return res.embeddings
 
+# Finding K nearest neighbours to enhance the answer.
+def topNNeighbours(promptEmbeddings: np.ndarray, storageEmbeddings: np.ndarray, df, k: int = 5):  
+	if isinstance(storageEmbeddings, list):  
+		storageEmbeddings = np.array(storageEmbeddings)  
+	if isinstance(promptEmbeddings, list):  
+		storageEmbeddings = np.array(promptEmbeddings)  
+	similarityMatrix = promptEmbeddings @ storageEmbeddings.T / np.outer(norm(promptEmbeddings, axis=-1), norm(storageEmbeddings, axis=-1))  
+	numNeighbours = min(similarityMatrix.shape[1], k)  
+	indices = np.argsort(similarityMatrix, axis=-1)[:, -numNeighbours:]
+	listOfStr=df.values.tolist()
+	neighbourValues:list=[]
+	for idx in indices[0]:
+		neighbourValues.append(listOfStr[idx])
+	return neighbourValues
 
+# Using the Cohere generate endpoint to return the answer into text data with additional options namely 'temperature' and 'max_tokens'.
+def generate(promptt, tmp, maxTokens):
+    res=co.generate(prompt=promptt, temperature=tmp, max_tokens=maxTokens)
+    return res
+
+# Using the streamlit library to create a user interface for better understanding.
 options=st.selectbox("Input type", ["PDF","TEXT"])
 
 if options=="PDF":
-    pdf_file=st.file_uploader("Upload file", type=["pdf"])
-    if pdf_file is not None:
-        text=extractTextFromPdf(pdf_file)
+    pdfFile=st.file_uploader("Upload file", type=["pdf"])
+    if pdfFile is not None:
+        text=extractTextFromPdf(pdfFile)
     if text is not None:
         df=processTextInput(text)
 elif options == "TEXT":  
@@ -51,7 +83,23 @@ elif options == "TEXT":
     if text is not None:  
         df = processTextInput(text)
 
-if text is not None:
-    # st.write(embed(df))
-    st.write(type(embed(convertToList(df))))
+if text!="":
+    listOfText=convertToList(df)
+    embeddings=embed(listOfText)
 
+if df is not None:
+    prompt=st.text_input('Ask a Question')
+    advancedOpt=st.checkbox('Advanced Options')
+    if advancedOpt is not None:
+        TEMPERATURE=st.slider('Temperature', min_value=0.0, max_value=1.0, value=TEMPERATURE)
+        MAX_TOKENS=st.slider('Max Tokens', min_value=1, max_value=5000, value=MAX_TOKENS)
+
+if df is not None and prompt != "":
+    basePrompt = "Based on the passage above, answer the following question:"
+    promptEmbeddings = embed([prompt])
+    augPrompts = topNNeighbours(np.array(promptEmbeddings), embeddings, df)
+    joinedPrompt = '\n'.join(str(neighbour) for neighbour in augPrompts) + '\n\n' + basePrompt + '\n' + prompt + '\n'
+    result = generate(joinedPrompt,TEMPERATURE,MAX_TOKENS)
+
+if result is not None: 
+    st.write(result.generations[0].text)
